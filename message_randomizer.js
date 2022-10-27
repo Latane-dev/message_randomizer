8014b86eae4a047ae1b8dfeb6052a15eda1f77f2
Latane S <Latanewrites@gmail.com>
2022-10-27 08:24:30 -0400

    Add function to put all the information into a sentence and log to console

--- message_randomizer.js
+++ message_randomizer.js
@@ -1,6 +1,6 @@
 /* First piece of information will be a randomly rolled numer 1-99 */
 const luckyNum = () => {
-    Math.floor(Math.random() * 100)
+    return Math.floor(Math.random() * 100)
 };
 
 /* Second piece of information will be a vague, opening relative cause made up using three arrays of words with a general nature theme */
@@ -11,7 +11,7 @@ const verbArr = ["runs wild", "sees the darkness", "smells a scent", "makes a no
 /* Third piece of information will be the main clause with an advice-like tone */
 
 const adviceArr = ["it is wise to go the other way", "do not hesitate to stand still", "keep a close eye on your enemies", 
-"your day will be filled with luck", "it is time for a big change in your life", "do not procrastinate to do work that can be accomplished in the present"]
+"your day will be filled with luck", "it is time for a big change in your life", "do not procrastinate what can be accomplished in the present"]
 
 /* function that will randomly choose a word or phrase from the array */
 const pickMe = (arr) => {
@@ -27,4 +27,11 @@ const capSentence = (stringVar) => {
     return stringVar.charAt(0).toUpperCase() + stringVar.slice(1);
 };
 
+/* this function will put together the random lucky number and fortune phrase */
+const fortune = () => {
+    const yourLuckyNum = capSentence(`your lucky number is ${luckyNum()}`);
+    const yourFortune = capSentence(`${relCause}, ${pickMe(adviceArr)}`)
+    return `${yourLuckyNum}. Today's fortune: ${yourFortune}.` 
+};
 
+console.log(fortune());
