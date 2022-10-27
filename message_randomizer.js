5a8892b37a3bb94080ceed8ee4f4c2ad63bada07
Latane S <Latanewrites@gmail.com>
2022-10-27 08:10:28 -0400

    Add variable for opening clause using its three sub array

--- message_randomizer.js
+++ message_randomizer.js
@@ -3,8 +3,8 @@ const luckyNum = () => {
     Math.floor(Math.random() * 100)
 };
 
-/* Second piece of information will be a vague, opening relative cause made up using arrays of words with a general nature theme */
-const firstWords = ["for", "when", "while", "until", "if"]
+/* Second piece of information will be a vague, opening relative cause made up using three arrays of words with a general nature theme */
+const firstWords = ["where", "when", "while", "until", "if"]
 const nounArr = ["the owl", "the horse", "the rabbit", "the pig", "the cat", "the dog", "the fish"]
 const verbArr = ["runs wild", "sees the darkness", "smells a scent", "makes a noise", "jumps to the sky", "hears danger", "sleeps soundly"]
 
@@ -19,4 +19,7 @@ const pickMe = (arr) => {
     return arr[ranPhrase];
 };
 
-console.log(pickMe(firstWords));
+/* defines the second piece of data (the relative opening clause) by combining a piece of data from its three arrray */
+let relCause = `${pickMe(firstWords)} ${pickMe(nounArr)} ${pickMe(verbArr)}`
+
+console.log(relCause);
